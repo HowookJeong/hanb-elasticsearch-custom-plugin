d75013a0e8490112c4d8fc49fea70ee227de95b8
HowookJeong <sophistlv@gmail.com>
2015-02-27 11:01:10 +0900

    Initialization.
    실무 예제로 배우는 Elasticsearch - 활용편

---
+++ src/main/java/hanb/elasticsearch/expert/plugin/custom/CustomPlugin.java
@@ -0,0 +1,20 @@
+package hanb.elasticsearch.expert.plugin.custom;
+
+import org.elasticsearch.common.inject.Module;
+import org.elasticsearch.plugins.AbstractPlugin;
+
+public class CustomPlugin extends AbstractPlugin {
+
+	@Override
+	public String description() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
